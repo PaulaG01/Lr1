c0f946a2f0fe8e3a437ef360d51c0738b617b4f2
Polina Ganebnaya <p.ganebnaya@syberry.com>
2023-05-28 19:55:12 +0300

    Initial commit

---
+++ app/src/main/java/bsuir/ganebnaya_polina/lr1/MainActivity.kt
@@ -0,0 +1,11 @@
+package bsuir.ganebnaya_polina.lr1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
