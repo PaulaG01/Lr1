c00dd2f1c57cc8dc579b07903ba7ea9eb8459303
paulaG01 <paula.g01@yandex.ru>
2023-05-28 20:11:04 +0300

    upd

--- app/src/main/java/bsuir/ganebnaya_polina/lr1/MainActivity.kt
+++ app/src/main/java/bsuir/ganebnaya_polina/lr1/MainActivity.kt
@@ -2,10 +2,21 @@ package bsuir.ganebnaya_polina.lr1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import bsuir.ganebnaya_polina.lr1.text.TextFunction
 
 class MainActivity : AppCompatActivity() {
+    //    TODO: Lr1 todo
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        extracted()
+    }
+
+    private fun extracted() {
+        val tf: TextFunction = TextFunction()
+        val nt: TextView = findViewById(R.id.newtest)
+        nt.text = tf.value
     }
 }
---
+++ app/src/main/java/bsuir/ganebnaya_polina/lr1/text/TextFunction.kt
@@ -0,0 +1,6 @@
+package bsuir.ganebnaya_polina.lr1.text
+
+class TextFunction {
+    val value: String
+        get() = "Hello from first project"
+}
